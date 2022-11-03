ccbe7f4e96f69eba6a53e28202e0d897837746a8
Dao Duong <dao.duong@saritasa.com>
2022-11-03 14:56:51 +0700

    add env for localStorage key

--- src/shared/constants/localStorageValue.ts
+++ src/shared/constants/localStorageValue.ts
@@ -1,3 +1,5 @@
+const isProduction = import.meta.env.VITE_APP_IS_PRODUCTION;
+
 export const LOCAL_STORAGE_KEY = {
-  watchTV: 'watchTV',
+  watchTV: isProduction ? 'watchTV' : 'watchTV-dev',
 };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_APP_API_BASE_URL: string;
   readonly VITE_APP_API_KEY: string;
   readonly VITE_APP_VIDEO_API_KEY: string;
+  readonly VITE_APP_IS_PRODUCTION: boolean;
 
   // more env variables...
 }
