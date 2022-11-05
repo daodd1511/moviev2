852252eadcedb4b4ef1241e0e5efa6be951a274e
DaoDuong <dao-duong@saritasa.com>
2022-11-05 12:51:26 +0700

    redeploy

--- src/shared/constants/localStorageValue.ts
+++ src/shared/constants/localStorageValue.ts
@@ -3,3 +3,5 @@ const isProduction = import.meta.env.VITE_APP_IS_PRODUCTION;
 export const LOCAL_STORAGE_KEY = {
   watchTV: isProduction ? 'watchTV' : 'watchTV-dev',
 };
+
+// Path: src\shared\constants\localStorageValue.ts
