cebe3dcaab835ae1ca44c70937921e0498d675b9
Dao Duong <dao.duong@saritasa.com>
2023-03-06 16:34:02 +0700

    Update token interceptor function

--- src/api/interceptors/token.interceptor.ts
+++ src/api/interceptors/token.interceptor.ts
@@ -10,6 +10,9 @@ import { TokenService } from '../services/tokenService';
 export function tokenInterceptor(
   config: AxiosRequestConfig,
 ): AxiosRequestConfig {
+  if (!shouldInterceptWithToken(config)) {
+    return config;
+  }
   const token = TokenService.get();
   if (token === null) {
     return config;
@@ -21,4 +24,12 @@ export function tokenInterceptor(
       Authorization: `Bearer ${token}`,
     },
   };
+}
+
+/**
+ * Should intercept with token.
+ * @param config Axios Request Config.
+ */
+export function shouldInterceptWithToken(config: AxiosRequestConfig): boolean {
+  return config.url?.startsWith('/auth') === false;
 }
