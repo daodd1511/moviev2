ebfc191e3a16c4ca10e4a3306711aba831d13d8f
Dao Duong <dao.duong@saritasa.com>
2023-03-22 10:35:27 +0700

    Add assert non null with return util

--- src/shared/utils/assertNonNull.ts
+++ src/shared/utils/assertNonNull.ts
@@ -10,3 +10,17 @@ export function assertNonNull<T>(val: T, message?: string): asserts val is NonNu
     );
   }
 }
+
+/**
+ * Check for null or undefined element with return value.
+ * @param val Optional type of value.
+ * @param message Optional message to display.
+ */
+export function assertNonNullWithReturn<T>(val: T, message?: string): NonNullable<T> {
+  if (val === undefined || val === null) {
+    throw new Error(
+      message ?? 'Expected \'element\' to be defined',
+    );
+  }
+  return val;
+}
